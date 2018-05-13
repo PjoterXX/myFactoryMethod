e6282782a53f01620eb73ceffd139a2cd4c19769
DominatorMK <DominatorMK@DESKTOP-B0JP85S>
2018-05-13 13:33:05 +0200

    - dodanie nowych klas reprezentujących badanie USG oraz RTG, - powiązanie wszystkich badań wspólną warstwą abstracji przy pomocy klasy MedicalService.

---
+++ src/factorymethod2/medicalServices/RTG.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorymethod2.medicalServices;
+
+/**
+ *
+ * @author LeopardProMK
+ */
+public class RTG  extends MedicalService{
+    /* Klasa reprezentująca badanie RTG */  
+}
